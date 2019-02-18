1d0a89b8937ce6ab45a787c311c0c0c37898bafd
Haunter17 <haunterhacker17@gmail.com>
2019-02-17 21:24:24 -0600

    new my_fun

--- MP1/plan_z/ncg_test.py
+++ MP1/plan_z/ncg_test.py
@@ -13,31 +13,49 @@ def ros_hess_fn(x):
 
 
 def my_fn(x):
-    return (x ** 4).sum()
+    fun = 0
+    for i in range(len(x) - 1):
+        fun += x[i]**4 + x[i + 1]**4
+    
+    return fun
 
 def my_jac_fn(x):
-    pdb.set_trace()
-    return np.array([
-        4 * x[0, 0]**3, 4 * x[0, 1]**3, 4 * x[1, 0]**3, 4 * x[1, 1]**3
-    ])
+    jac = np.empty((len(x),))
+    for i in range(len(x)):
+        if i == 0:
+            jac[i] = 4 * x[i]**3 * x[i + 1]**4
+        elif i == len(x) - 1:
+            jac[i] = 4 * x[i]**3 * x[i - 1]**4
+        else:
+            jac[i] = 4 * x[i]**3 * (x[i - 1]**4 + x[i + 1]**4)
+
+    return jac
 
 def my_hess_fn(x):
-    return np.array([
-        [12 * x[0, 0]**2, 0, 0, 0],
-        [0, 12 * x[0, 1]**2, 0, 0],
-        [0, 0, 12 * x[1, 0]**2, 0],
-        [0, 0, 0, 12 * x[1, 1]**2]
-    ])
+    hess = np.zeros((len(x), len(x)))
+    for i in range(len(x)):
+        if i == 0:
+            hess[i, i] = 12 * x[i]**2 * x[i + 1]**4
+        elif i == len(x) - 1:
+            hess[i, i] = 12 * x[i]**2 * x[i - 1]**4
+        else:
+            hess[i, i] = 12 * x[i]**2 * (x[i - 1]**4 + x[i + 1]**4)
+
+        if i != len(x) - 1:
+            hess[i, i + 1] = hess[i + 1, i] = 16 * x[i]**3 * x[i + 1]**3
+
+    return hess
+
 
 if __name__ == '__main__':
-    x = np.array([2, -1])
+    x = np.random.randn(10)
     #  x = np.array([
     #      [1, -1],
     #      [-1, 1]
     #  ])
     pdb.set_trace()
     #  res = optimize.minimize(ros_fn, x, method='Newton-CG', jac=ros_jac_fn, hess=ros_hess_fn)
-    res = optimize.fmin_ncg(ros_fn, x, fprime=ros_jac_fn, fhess=ros_hess_fn, full_output=True, retall=True)
+    res = optimize.fmin_ncg(my_fn, x, fprime=my_jac_fn, fhess=my_hess_fn, full_output=False, retall=False)
     pdb.set_trace()
 
 
